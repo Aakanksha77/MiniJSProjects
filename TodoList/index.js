8ecab026c63b4ee2d99f8c3da159a17c649daec1
Aakanksha77 <akanksharajbhar77@gmail.com>
2024-09-29 20:43:46 +0530

    Complete with Js Part

--- TodoList/index.js
+++ TodoList/index.js
@@ -3,7 +3,7 @@ const listContainer = document.getElementById("list-container");
 
 function addTask(){
    if(inputBox.value === ''){
-        alert("Pleas add your Task")
+        alert("Please add your Task!")
     }
    else{
         let li = document.createElement('li');
@@ -15,14 +15,26 @@ function addTask(){
         li.appendChild(span)
    }
    inputBox.value = "";
+   saveData();
 }
 
 
 listContainer.addEventListener("click", function(e){
     if (e.target.tagName === 'LI') {
         e.target.classList.toggle("checked");
+        saveData();
     }
     else if(e.target.tagName === "SPAN"){
         e.target.parentElement.remove();
+        saveData();
     }
-})
+},false)
+
+function saveData(){
+    localStorage.setItem("data", listContainer.innerHTML);
+}
+
+function showTask(){
+    listContainer.innerHTML = localStorage.getItem("data");
+}
+showTask();
